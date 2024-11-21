22bf62eb352a4187caae52e9d7b625297e7dcfcc
cyb <ybchen@leapcloud.cn>
2024-11-21 09:45:57 +0800

    support chinese fonts

--- scale_build/image/iso.py
+++ scale_build/image/iso.py
@@ -34,6 +34,13 @@ def install_iso_packages_impl():
     # echo "/dev/disk/by-label/TRUENAS / iso9660 loop 0 0" > ${CHROOT_BASEDIR}/etc/fstab
     for package in get_manifest()['iso-packages']:
         run_in_chroot(['apt', 'install', '-y', package])
+    
+
+    run_in_chroot(['sed', '-i', 's/# zh_CN.UTF-8 UTF-8/zh_CN.UTF-8 UTF-8/', '/etc/locale.gen'])
+
+    run_in_chroot(['locale-gen'])
+
+    run_in_chroot(['update-locale', 'LANG=zh_CN.UTF-8'])
 
     # Inject vendor name into grub.cfg
     with open(CONF_GRUB, 'r') as f:
@@ -116,7 +123,7 @@ def make_iso_file():
         run_in_chroot(['apt-get', 'update'], check=False)
         run_in_chroot([
             'apt-get', 'install', '-y', 'grub-common', 'grub2-common', 'grub-efi-amd64-bin',
-            'grub-efi-amd64-signed', 'grub-pc-bin', 'mtools', 'xorriso'
+            'grub-efi-amd64-signed', 'grub-pc-bin', 'mtools', 'xorriso','fonts-wqy-zenhei'
         ])
 
         # Debian GRUB EFI searches for GRUB config in a different place
@@ -125,6 +132,22 @@ def make_iso_file():
         os.makedirs(os.path.join(CD_DIR, 'EFI/debian/fonts'), exist_ok=True)
         shutil.copy(os.path.join(CHROOT_BASEDIR, 'usr/share/grub/unicode.pf2'),
                     os.path.join(CD_DIR, 'EFI/debian/fonts/unicode.pf2'))
+        
+        os.makedirs(os.path.join(CHROOT_BASEDIR, 'boot/grub/fonts'), exist_ok=True)
+
+        run_in_chroot([
+            'grub-mkfont', 
+            '-o', '/boot/grub/fonts/wqy-zenhei.pf2',
+            '-s', '16',
+            '/usr/share/fonts/truetype/wqy/wqy-zenhei.ttc'
+        ])
+        
+        shutil.copy(os.path.join(CHROOT_BASEDIR, 'boot/grub/fonts/wqy-zenhei.pf2'),
+                    os.path.join(CD_DIR, 'EFI/debian/fonts/wqy-zenhei.pf2'))
+        
+        shutil.copy(os.path.join(CHROOT_BASEDIR, 'boot/grub/fonts/wqy-zenhei.pf2'),
+            os.path.join(CD_DIR, 'boot/grub/fonts/wqy-zenhei.pf2'))
+
 
         iso = os.path.join(RELEASE_DIR, f'TrueNAS-SCALE-{get_image_version(vendor=TRUENAS_VENDOR)}.iso')
 
